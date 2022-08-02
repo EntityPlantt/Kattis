e62140c6524f7d6ddc393ea71f31b9fd86af1ebd
EntityPlantt <92634082+EntityPlantt@users.noreply.github.com>
2022-08-02 15:30:45 +0200

    Added countthevowels

---
+++ countthevowels.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+main() {
+    string x;
+    getline(cin, x);
+    cout << count(x.begin(), x.end(), 'A') + count(x.begin(), x.end(), 'E')
+    + count(x.begin(), x.end(), 'I') + count(x.begin(), x.end(), 'O')
+    + count(x.begin(), x.end(), 'U') + count(x.begin(), x.end(), 'a')
+    + count(x.begin(), x.end(), 'e') + count(x.begin(), x.end(), 'i')
+    + count(x.begin(), x.end(), 'o') + count(x.begin(), x.end(), 'u');
+    return 0;
+}
