1d766d00934858f559216b1c468446b07ff7d369
EntityPlantt <92634082+EntityPlantt@users.noreply.github.com>
2022-08-02 15:30:27 +0200

    Added cd

---
+++ cd.cpp
@@ -0,0 +1,37 @@
+// Time Limit Exceeded
+#include <iostream>
+#include <vector>
+using namespace std;
+main() {
+    size_t n, m, x, t, i;
+    while (true) {
+        cin >> n >> m;
+        if (!n && !m) {
+            break;
+        }
+        t = i = 0;
+        vector <size_t> jack;
+        while (n--) {
+            cin >> x;
+            jack.push_back(x);
+        }
+        while (m--) {
+            cin >> x;
+            if (i >= jack.size()) {
+                break;
+            }
+            while (jack[i] < x) {
+                i++;
+                if (i >= jack.size()) {
+                    break;
+                }
+            }
+            if (i >= jack.size()) {
+                break;
+            }
+            t += jack[i] == x;
+        }
+        cout << t << '\n';
+    }
+    return 0;
+}
