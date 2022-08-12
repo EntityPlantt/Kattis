97c2823b989e84324b07a96c3329a5a1e7c9001a
EntityPlantt <92634082+EntityPlantt@users.noreply.github.com>
2022-08-12 15:25:11 +0200

    Update listgame.js
    Convert indentations to tabs

--- listgame.js
+++ listgame.js
@@ -1,15 +1,15 @@
 // GitHub: EntityPlantt/Kattis
 require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 }).on("line", n => {
-    var o = 0;
-    n = parseInt(n);
-    for (var i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            n /= i--;
-            o++;
-        }
-    }
-    console.log(++o);
+	var o = 0;
+	n = parseInt(n);
+	for (var i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			n /= i--;
+			o++;
+		}
+	}
+	console.log(++o);
 });
